0ab7cf978a4d698d273767efc43d332a6922374b
Qori El-Hafizh <contact@qori.dev>
2022-11-14 18:23:47 +0700

    compiled

--- main.c
+++ main.c
@@ -4,20 +4,71 @@
 #include <string.h>
 
 #define INITIAL_SIZE 30000
+#define INSTRUCTION_INITIAL_SIZE 64
+
+typedef enum
+{
+  OP_MOVE,
+  OP_ADD,
+  OP_BEGIN_LOOP,
+  OP_END_LOOP,
+  OP_IN,
+  OP_OUT,
+  OP_END,
+} Op;
+
+typedef struct
+{
+  Op op;
+  int payload;
+} Instruction;
+
+typedef struct
+{
+  size_t length;
+  size_t capacity;
+  Instruction* array;
+} InstructionArray;
+
+InstructionArray
+new_instruction_array()
+{
+  Instruction* array = calloc(INSTRUCTION_INITIAL_SIZE, sizeof(Instruction));
+  InstructionArray instructions = {
+    .length = 0,
+    .capacity = INSTRUCTION_INITIAL_SIZE,
+    .array = array,
+  };
+  return instructions;
+}
+
+void
+push_instruction(InstructionArray* array, Instruction value)
+{
+  if (array->length == array->capacity) {
+    array->array =
+      realloc(array->array, sizeof(Instruction) * array->capacity * 2);
+
+    array->capacity *= 2;
+  }
+
+  array->array[array->length] = value;
+  array->length++;
+}
 
 typedef unsigned int Cell;
 typedef struct
 {
-  Cell* array;
   size_t size;
   size_t current;
+  Cell* array;
 } MemoryArray;
 
 MemoryArray
 new_memory_array()
 {
   int* array = calloc(INITIAL_SIZE, sizeof(int));
-  MemoryArray mem = (MemoryArray){
+  MemoryArray mem = {
     .size = INITIAL_SIZE,
     .current = 0,
     .array = array,
@@ -33,25 +84,6 @@ free_memory_array(MemoryArray* mem)
   mem->array = NULL;
 }
 
-int
-get_current_value(MemoryArray* mem)
-{
-  int curr = mem->current;
-  return mem->array[curr];
-}
-
-void
-inc(MemoryArray* mem)
-{
-  mem->array[mem->current]++;
-}
-
-void
-dec(MemoryArray* mem)
-{
-  mem->array[mem->current]--;
-}
-
 void
 grow_to_right(MemoryArray* mem)
 {
@@ -60,55 +92,34 @@ grow_to_right(MemoryArray* mem)
 }
 
 void
-grow_to_left(MemoryArray* mem)
+move(MemoryArray* mem, int count)
 {
+  mem->current += count;
 
-  size_t old_size = mem->size;
-  mem->size *= 2;
-  mem->array = realloc(mem->array, mem->size * sizeof(Cell));
-
-  memmove(mem->array + old_size, mem->array, sizeof(Cell) * old_size);
-  memset(mem->array, 0, sizeof(Cell) * old_size);
-  mem->current += old_size;
-}
-
-void
-next(MemoryArray* mem)
-{
-  if (mem->current >= mem->size - 1) {
+  while (mem->current >= mem->size) {
     grow_to_right(mem);
   }
-  mem->current++;
-}
-
-void
-prev(MemoryArray* mem)
-{
-  if (mem->current <= 0) {
-    grow_to_left(mem);
-  }
-  mem->current--;
 }
 
 size_t
-jump_forward(const char* source, size_t i)
+jump_forward(Instruction* instructions, size_t i)
 {
   i++;
 
   int count = 0;
   for (;;) {
-    if (source[i] == '\0') {
+    if (instructions[i].op == OP_END) {
       break;
     }
 
-    if (source[i] == ']' && count == 0) {
+    if (instructions[i].op == OP_END_LOOP && count == 0) {
       i++;
       break;
     }
 
-    if (source[i] == '[') {
+    if (instructions[i].op == OP_BEGIN_LOOP) {
       count++;
-    } else if (source[i] == ']') {
+    } else if (instructions[i].op == OP_END_LOOP) {
       count--;
     }
 
@@ -119,7 +130,7 @@ jump_forward(const char* source, size_t i)
 }
 
 size_t
-jump_back(const char* source, size_t i)
+jump_back(Instruction* instructions, size_t i)
 {
   i--;
 
@@ -129,14 +140,14 @@ jump_back(const char* source, size_t i)
       break;
     }
 
-    if (source[i] == '[' && count == 0) {
+    if (instructions[i].op == OP_BEGIN_LOOP && count == 0) {
       i++;
       break;
     }
 
-    if (source[i] == ']') {
+    if (instructions[i].op == OP_END_LOOP) {
       count++;
-    } else if (source[i] == '[') {
+    } else if (instructions[i].op == OP_BEGIN_LOOP) {
       count--;
     }
 
@@ -147,46 +158,40 @@ jump_back(const char* source, size_t i)
 }
 
 void
-interpret(const char* source, MemoryArray* mem)
+interpret(InstructionArray instructions)
 {
-  for (size_t i = 0; i < strlen(source);) {
-    switch (source[i]) {
-      case '>':
-        next(mem);
+  MemoryArray mem = new_memory_array();
+  for (size_t i = 0; i < instructions.length;) {
+    // printf("%d %ld\n", instructions.array[i].op, i);
+    switch (instructions.array[i].op) {
+      case OP_MOVE:
+        move(&mem, instructions.array[i].payload);
         i++;
         break;
-      case '<':
-        prev(mem);
+      case OP_ADD:
+        mem.array[mem.current] += instructions.array[i].payload;
         i++;
         break;
-      case '+':
-        inc(mem);
-        i++;
-        break;
-      case '-':
-        dec(mem);
-        i++;
-        break;
-      case '.':
-        putchar(get_current_value(mem));
-        i++;
-        break;
-      case ',':
-        mem->array[mem->current] = getchar();
-        i++;
-        break;
-      case '[':
-        if (get_current_value(mem) == 0) {
-          i = jump_forward(source, i);
+      case OP_BEGIN_LOOP:
+        if (mem.array[mem.current] == 0) {
+          i = jump_forward(instructions.array, i);
           break;
         }
         i++;
         break;
-      case ']':
-        if (get_current_value(mem) != 0) {
-          i = jump_back(source, i);
+      case OP_END_LOOP:
+        if (mem.array[mem.current] != 0) {
+          i = jump_back(instructions.array, i);
           break;
         }
+        i++;
+        break;
+      case OP_IN:
+        mem.array[mem.current] = getchar();
+        i++;
+        break;
+      case OP_OUT:
+        putchar(mem.array[mem.current]);
         i++;
         break;
       default:
@@ -196,26 +201,135 @@ interpret(const char* source, MemoryArray* mem)
   }
 }
 
-void
-repl()
+size_t
+instruction_move(InstructionArray* array, const char* source, size_t i)
 {
-  char source[1024];
-  MemoryArray mem = new_memory_array();
-  for (;;) {
-    printf("> ");
 
-    if (!fgets(source, sizeof(source), stdin)) {
-      printf("\n");
-      break;
-    }
-
-    if (strcmp(source, "q\n") == 0) {
-      break;
-    }
-    interpret(source, &mem);
-    printf("\n");
+  int payload = 0;
+  while (source[i] == '>' || source[i] == '<') {
+    payload += (source[i] == '>') ? 1 : -1;
+    i++;
   }
-  free_memory_array(&mem);
+
+  Instruction inst = {
+    .op = OP_MOVE,
+    .payload = payload,
+  };
+
+  push_instruction(array, inst);
+  return i;
+}
+
+size_t
+instruction_add(InstructionArray* array, const char* source, size_t i)
+{
+
+  int payload = 0;
+  while (source[i] == '+' || source[i] == '-') {
+    payload += (source[i] == '+') ? 1 : -1;
+    i++;
+  }
+
+  Instruction inst = {
+    .op = OP_ADD,
+    .payload = payload,
+  };
+
+  push_instruction(array, inst);
+  return i;
+}
+
+size_t
+instruction_in(InstructionArray* array, size_t i)
+{
+  int payload = 0;
+
+  Instruction inst = {
+    .op = OP_IN,
+    .payload = payload,
+  };
+
+  push_instruction(array, inst);
+  return i + 1;
+}
+
+size_t
+instruction_out(InstructionArray* array, size_t i)
+{
+  int payload = 0;
+
+  Instruction inst = {
+    .op = OP_OUT,
+    .payload = payload,
+  };
+
+  push_instruction(array, inst);
+  return i + 1;
+}
+
+size_t
+instruction_begin_loop(InstructionArray* array, size_t i)
+{
+  int payload = 0;
+
+  Instruction inst = {
+    .op = OP_BEGIN_LOOP,
+    .payload = payload,
+  };
+
+  push_instruction(array, inst);
+  return i + 1;
+}
+
+size_t
+instruction_end_loop(InstructionArray* array, size_t i)
+{
+  int payload = 0;
+
+  Instruction inst = {
+    .op = OP_END_LOOP,
+    .payload = payload,
+  };
+
+  push_instruction(array, inst);
+  return i + 1;
+}
+
+InstructionArray
+scan(const char* source)
+{
+  InstructionArray array = new_instruction_array();
+  for (size_t i = 0; i < strlen(source);) {
+    switch (source[i]) {
+      case '>':
+      case '<':
+        i = instruction_move(&array, source, i);
+        break;
+      case '+':
+      case '-':
+        i = instruction_add(&array, source, i);
+        break;
+      case '.':
+        i = instruction_out(&array, i);
+        break;
+      case ',':
+        i = instruction_in(&array, i);
+        break;
+      case '[':
+        i = instruction_begin_loop(&array, i);
+        break;
+      case ']':
+        i = instruction_end_loop(&array, i);
+        break;
+      default:
+        i++;
+        break;
+    }
+  }
+  Instruction end_instruction = { .op = OP_END, .payload = 0 };
+  push_instruction(&array, end_instruction);
+
+  return array;
 }
 
 int
@@ -223,7 +337,8 @@ main(int argc, char const* argv[])
 {
 
   if (argc == 1) {
-    repl();
+    // repl();
+    exit(0);
   } else if (argc == 2) {
 
     char* source = 0;
@@ -237,9 +352,9 @@ main(int argc, char const* argv[])
       source = malloc(length);
       if (source) {
         fread(source, 1, length, f);
-        MemoryArray mem = new_memory_array();
-        interpret(source, &mem);
-        free_memory_array(&mem);
+
+        InstructionArray instructions = scan(source);
+        interpret(instructions);
       }
       fclose(f);
     }
